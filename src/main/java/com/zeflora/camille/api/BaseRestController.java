82d3c23799d8687227151587c12a6f1837c76a19
zephyr <zefeng.lin@anaplan.com>
2018-07-22 22:16:00 -0700

    Added a view count to hello endpoint

--- src/main/java/com/zeflora/camille/api/BaseRestController.java
+++ src/main/java/com/zeflora/camille/api/BaseRestController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by Zephyr Lin
  * User: zephyr
@@ -15,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseRestController {
     private static final Logger LOG = LoggerFactory.getLogger(BaseRestController.class);
 
+    private AtomicInteger callCnt = new AtomicInteger(0);
+
     @RequestMapping(value = "/v1/hello")
     public String hello() {
-        return "Hello";
+        String ret = "Hello, called " + callCnt.incrementAndGet() + " times";
+        LOG.info(ret);
+        return ret;
     }
 }
